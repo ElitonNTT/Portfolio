8e6fa1c657f77c2b701246637a5268d092c5373c
Eliton Netto <netinho_ntt@hotmail.com>
2025-03-26 15:01:16 -0300

    fix: packagejson and viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/Portfolio-Netto/",
+  base: "/portfolio/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
