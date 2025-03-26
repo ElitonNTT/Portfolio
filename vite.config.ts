d7fb507f6bab308a1d6166cb187c03929e604972
Eliton Netto <netinho_ntt@hotmail.com>
2025-03-26 14:49:58 -0300

    chore: viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/Portfolio-Netto",
+  base: "/Portfolio-Netto/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
